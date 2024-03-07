e3f229ce7da3d910d13fd0cce4d6c5d8685582c6
yeeun03030 <skdPdms04@gmail.com>
2024-03-07 15:57:01 +0900

    Activity 추가3

---
+++ app/src/main/java/kr/ac/kopo/skdPdms04/MainActivity3.kt
@@ -0,0 +1,11 @@
+package kr.ac.kopo.skdPdms04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
